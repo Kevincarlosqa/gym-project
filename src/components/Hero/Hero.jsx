0dd2bcf38455ac8ea9c94760d621389467731d46
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-06 17:52:13 -0500

    add: Hero banner

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,6 +8,28 @@ const Hero = () => {
     <div className="hero">
       <div className="left-h">
         <Header />
+        {/* The best ad */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>The best Social Fitness in Aqp</span>
+        </div>
+
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal Body</span>
+          </div>
+          <div>
+            <span>
+              Aquí te ayudaremos a moldear y construir tu cuerpo ideal y vivir
+              tu vida al máximo
+            </span>
+          </div>
+        </div>
       </div>
       <div className="right-h">right side</div>
     </div>
